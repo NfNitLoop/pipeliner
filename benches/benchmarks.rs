12696edd8bdd32f2b6428be3dfe9395d6ac60f80
Cody Casterline <cody.casterline@gmail.com>
2018-11-18 20:37:16 +0000

    Simple benchmark for ordered lists.
    Results:
    test sending_and_receiving         ... bench:  24,264,924 ns/iter (+/- 23,630,790)
    test sending_and_receiving_ordered ... bench:  60,652,994 ns/iter (+/- 10,448,337)
    
    Which means that, while ordered operations are slower than unordered,
    they're still about twice as fast as the old implementation before
    crossbeam_channel.

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -9,8 +9,7 @@ extern crate pipeliner;
 use pipeliner::Pipeline;
 
 // This benchmark just tests passing through some data unmodified.
-// We're really testing the underlying channel implementation.
-// (And: currently, sync::Mutex I've added to simulate spmc. Sorry. :p)
+// We're really testing the underlying channel implementation.)
 #[bench]
 fn sending_and_receiving(b: &mut Bencher) {
     let iterations = black_box(10_000);
@@ -19,6 +18,22 @@ fn sending_and_receiving(b: &mut Bencher) {
     b.iter(|| {
         let mut count = 0;
         for _ in (0..iterations).with_threads(threads).map(|x| x) {
+            count += 1;
+        }
+
+        assert_eq!(iterations, count);
+        println!("Foobar {}", iterations);
+    });
+}
+
+#[bench]
+fn sending_and_receiving_ordered(b: &mut Bencher) {
+    let iterations = black_box(10_000);
+    let threads = black_box(4);
+
+    b.iter(|| {
+        let mut count = 0;
+        for _ in (0..iterations).with_threads(threads).ordered_map(|x| x) {
             count += 1;
         }
 
